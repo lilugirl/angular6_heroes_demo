85cbe8f096a8f7c5141223de4c36500cac82f808
lilu <lilugirl2005@126.com>
2018-10-06 16:18:22 +0800

    init

---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+  constructor(public MessageService: MessageService) {
+    console.log('message.component', MessageService);
+    console.log('length', MessageService.messages.length)
+  }
+
+  ngOnInit() {
+  }
+
+}
